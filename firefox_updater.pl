54cc5201f6e9c7c7ff74ea178abe7d50613f9b94
Dave Webb <d5ve.github.com@dcw.co.nz>
2011-02-01 16:20:01 +0000

    DCW - Version 1.0 - Fully Functioning!
    Huge raft of new code to get everything working.
    
    FTP was split out into a subroutine, as we do basically the same
    operations for fetching the latest build ID, as we do for fetching the
    latest build itself.
    
    This all seems to work on my xubuntu host, and should work with minor
    tweaking on OSX.

--- firefox_updater.pl
+++ firefox_updater.pl
@@ -9,97 +9,198 @@ Firefox Updater - fetch and install firefox nightly builds
 
 =head2 DESCRIPTION
 
-Firefox Updater is a perl script which fetches Firefox trunk nightly builds and installs them.
+Firefox Updater is a perl script which fetches Firefox trunk nightly builds and
+installs them.
 
-!!This script isn't yet working!!
-
-So far only fetching the latest daily build has been implemented.
+This script checks the currently installed version against the latest one
+hosted at ftp.mozilla.org. If a new version is found, and firefox isn't
+currently running, then the new version is downloaded, unpacked and installed.
+This completely replaces the old version's files, but settings, history, and
+bookmarks are safe, as they're stored outside the application directory.
 
 =head2 SYNOPSIS
 
-    # Check for a new daily build and install.
+    # Check for a new daily build and install if one is found.
     firefox_updater.pl
 
 =head2 VERSION
 
-version 0.01 - 2011-01-27
+version 1.00 - 2011-02-01
 
-    Now hosted on github - http://github.com/d5ve/firefox-updater
+    Now works from end to end.
 
 =cut
 
 use Data::Dumper;
+use File::Copy ();
+use File::Path ();
+use File::Spec ();
+use File::Temp ();
 use Net::FTP;
-use File::Temp;
-use File::Copy;
+
+my $debug = 0;
 
 # Fetching details
 my $ftp_host = 'ftp.mozilla.org';
 my $ftp_dir  = '/pub/mozilla.org/firefox/nightly/latest-trunk/';
-my $nightly_file_suffix = 'linux-i686.tar.bz2';
-my $nightly_file_suffix = 'linux-i686.tar.bz2';
+my $nightly_build_file_suffix   = 'linux-i686.tar.bz2';
+my $nightly_version_file_suffix = 'linux-i686.txt';
 
 # Local machine details
-my $application_dir = "$ENV{HOME}/Applications/firefox";
+my $base_name               = 'firefox';
+my $application_root_dir    = "$ENV{HOME}/Applications";
+my $application_dir         = "$application_root_dir/$base_name";
+my $process_name            = "$application_dir/firefox-bin";
+my $temp_dir                = File::Spec->tmpdir;
+my $unpack_command          = "tar -C $temp_dir -xf";
 
-my $ftp = Net::FTP->new($ftp_host, Debug => 0)
-    or die "Cannot connect to '$ftp_host': $@";
-$ftp->login("anonymous",'-anonymous@')
-    or die "Cannot login ", $ftp->message;
-$ftp->cwd($ftp_dir)
-    or die "Cannot change working directory to '$ftp_dir'", $ftp->message;
-
-my @files = $ftp->ls();
-my ($nightly_build_file) = grep { m{ $nightly_file_suffix \z }xms } @files;
-
-if ( ! $nightly_build_file ) {
-    print "Error: Unable to find nightly file matching '$nightly_file_suffix' in:\n\t" . join("\n\t", @files) . "\nExiting...\n";
-    exit 1;
+# Check for a newer version
+my $current_build_id = get_current_build_id();
+my $latest_build_id = get_latest_build_id();
+print "Current build ID: $current_build_id, Latest build ID: $latest_build_id\n";
+if ( $current_build_id < $latest_build_id ) {
+    # Check to see if firefox is currently running,
+    if ( is_firefox_running() ) {
+        print "Firefox is currently running. Please stop it first. Exiting...\n";
+        exit 10;
+    }
+    else {
+        # Fetch and install new build
+        fetch_and_install_latest_build();
+    }
 }
-
-my $local_tmp = File::Temp->new();
-my $local_tmp_filename = $local_tmp->filename;
-
-print "Fetching nightly file '$nightly_build_file' as '$local_tmp_filename'\n";
-    
-$ftp->binary() or die "Unable to set binary mode for file transfer", $ftp->message;
-$ftp->get($nightly_build_file, $local_tmp_filename)
-    or die "Failed to get '$nightly_build_file' -> '$local_tmp_filename'", $ftp->message;
-
-$ftp->quit;
-
-copy($local_tmp_filename, '/tmp/firefox_nightly.bz2');
-
-#system "cd /tmp; tar xf $local_tmp_filename";
+else {
+    print "No new build found. Exiting...\n";
+    exit 20;
+}
 
 exit;
 
-sub get_current_version {
+# Check whether firefox is already running.
+# Probably should use /proc.
+sub is_firefox_running {
+
+    my $command = qq{ ps auxww | grep -v grep | grep "$process_name" | awk '{print \$2}' }; # vim's syntax highlighting sucks.
+    my $pid = `$command`;
+    chomp $pid;
+
+    return $pid;
+}
+
+# Fetch the latest build from the FTP repos.
+sub fetch_and_install_latest_build {
+    my $local_tmp          = File::Temp->new();
+    my $local_tmp_filename = $local_tmp->filename;
+
+    print "Fetching latest build\n";
+    fetch_file_via_ftp($nightly_build_file_suffix, $local_tmp_filename);
+
+    # Unpack the new build.
+    print "Unpacking latest build\n";
+    system("$unpack_command $local_tmp_filename");
+
+
+    # Make sure application dir exists
+    File::Path::make_path($application_dir) unless -d $application_dir;
+
+    # Remove any existing install
+    File::Path::remove_tree($application_dir) or die "ERROR: Unable to remove current install '$application_dir' - $!"; 
+
+    # Move the new build into place
+    File::Copy::move("$temp_dir/$base_name", $application_dir) or die "ERROR: Unable to move '$temp_dir/$base_name' to '$application_dir' - $!";
+
+}
+
+sub fetch_file_via_ftp {
+    my $file_suffix = shift or die 'fetch_file_via_ftp() missing required $file_suffix param. Exiting...';
+    my $local_filename = shift or die 'fetch_file_via_ftp() missing required $local_filename param. Exiting...';
+
+    my $ftp = Net::FTP->new($ftp_host, Debug => 0)
+        or die "Cannot connect to '$ftp_host': $@";
+    $ftp->login("anonymous",'-anonymous@')
+        or die "Cannot login ", $ftp->message;
+    $ftp->cwd($ftp_dir)
+        or die "Cannot change working directory to '$ftp_dir'", $ftp->message;
+
+    my @files = $ftp->ls();
+    my ($requested_file) = grep { m{ $file_suffix \z }xms } @files;
+
+    if ( ! $requested_file ) {
+        print "Error: Unable to find nightly file matching '$file_suffix' in:\n\t" . join("\n\t", @files) . "\nExiting...\n";
+        exit 1;
+    }
+
+    print "Fetching file '$requested_file' as '$local_filename'\n" if $debug;
+
+    $ftp->binary() or die "Unable to set binary mode for file transfer", $ftp->message;
+    $ftp->get($requested_file, $local_filename)
+        or die "Failed to get '$requested_file' -> '$local_filename'", $ftp->message;
+
+    $ftp->quit;
+}
+
+# Read the latest build ID from the FTP repository
+sub get_latest_build_id {
+    my $latest_build_id = 0;
+    my $local_tmp          = File::Temp->new();
+    my $local_tmp_filename = $local_tmp->filename;
+
+    fetch_file_via_ftp($nightly_version_file_suffix, $local_tmp_filename);
+
+    open my $version_fh, '<', $local_tmp_filename or die "ERROR: Unable to open version file '$local_tmp_filename' for reading - $!";
+    while ( my $line = <$version_fh> ) {
+        # 20110201030339
+        # http://hg.mozilla.org/mozilla-central/rev/8b5cb26bbb10
+        print "DEBUG: $line" if $debug;
+        if ( $line =~ m{ \A (\d{10,}) \s* \z }xms ) {
+            $latest_build_id = $1;
+            last;
+        }
+    }
+    close $version_fh or die "ERROR: Unable to close version file '$local_tmp_filename' after reading - $!";
+
+    return $latest_build_id;
+
+}
+
+# Read the build ID from the currently installed firefox.
+sub get_current_build_id {
     my $version_file = "$application_dir/application.ini";
 
-    open my $version_fh, '<', $version_file or die "ERROR: Unable to open version file '$version_file' for reading - $!";
-    
+    my $current_build_id = 0;
+    my $version_fh;
+    open($version_fh, '<', $version_file) or do {
+        print "WARNING: Unable to open version file '$version_file' for reading - $!\n";
+        print "         Assuming that this is the first run of the script\n";
+        return $current_build_id;
+    };
+
     while ( my $line = <$version_fh> ) {
         #BuildID=20110127030333
-        if ( $line =~ m{ \A BuildID=(\d+) \z }xms ) {
-            
+        if ( $line =~ m{ \A BuildID=(\d+) \s* \z }xms ) {
+            $current_build_id = $1;
+            last;
         }
     }
 
     close $version_fh or die "ERROR: Unable to close version file '$version_file' after reading - $!";
+
+    return $current_build_id;
 }
 
 =head2 TODO
 
 =over 4
 
-=item * Check that firefox isn't currently running.
-
-=item * Check the version number of the nightly is different to that installed.
-
-=item * Install the new version.
+=item * Test on OSX
 
 =back
+
+=head2 HISTORY
+
+version 0.01 - 2011-01-27
+
+    Now hosted on github - http://github.com/d5ve/firefox-updater
 
 =head2 AUTHOR
 
